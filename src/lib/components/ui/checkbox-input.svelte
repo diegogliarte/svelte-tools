70245a99b45c38947b8eb831249069c2dd788427
Diego González Liarte <diegogliarte@gmail.com>
2025-12-21 23:39:21 +0100

    add check icon to checkbox-input

--- src/lib/components/ui/checkbox-input.svelte
+++ src/lib/components/ui/checkbox-input.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import MdiCheck from '~icons/mdi/check';
+
 	interface Props {
 		label?: string;
 		checked?: boolean;
@@ -12,20 +14,39 @@
 	const uid = $props.id();
 </script>
 
-<label for={uid} class="flex items-center gap-1 cursor-pointer text-xs h-fit group">
-	<input
-		id={uid}
-		type="checkbox"
-		bind:checked
-		class="
-			w-3 h-3
-			appearance-none
-			border
-			checked:bg-accent-dark
-			checked:border-accent
-			cursor-pointer
-			transition
-		"
-	/>
-	<span class="transition group-hover:text-accent">{label}</span>
+<label
+	for={uid}
+	class="flex items-center gap-1 cursor-pointer text-xs h-fit group select-none"
+>
+	<span class="relative w-3 h-3 flex items-center justify-center">
+		<input
+			id={uid}
+			type="checkbox"
+			bind:checked
+			class="
+				w-3 h-3
+				appearance-none
+				border
+				checked:bg-accent-dark
+				checked:border-accent
+				cursor-pointer
+				transition
+			"
+		/>
+
+		<MdiCheck
+			class="
+				absolute
+				w-2.5 h-2.5
+				text-accent
+				pointer-events-none
+				transition
+				{checked ? 'opacity-100 ' : 'opacity-0'}
+			"
+		/>
+	</span>
+
+	<span class="transition group-hover:text-accent">
+		{label}
+	</span>
 </label>
